b5fabfa0db02a34dd3e2ac49bf6ef90370899742
C. Moss <caly.moss@gmail.com>
2014-06-09 17:52:43 -0700

    all solutions with Boris

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,17 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var newBlinkyDancer = new Dancer;
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of newBlinkyDancer function
+  Dancer.call(this, top, left, timeBetweenSteps);
+  
+};
 
-  var oldStep = newBlinkyDancer.step;
+BlinkyDancer.prototype = new Dancer();
 
-  newBlinkyDancer.step = function(){
-    // call the old version of step at the beginning of any call to newBlinkyDancer new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for newBlinkyDancer and
-    // other effects you can use on a jQuery-wrapped html tag.
-    newBlinkyDancer.$node.toggle();
-  };
-  newBlinkyDancer.step();
-  return newBlinkyDancer;
+  
+BlinkyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to newBlinkyDancer new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for newBlinkyDancer and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -5,12 +5,8 @@ var Dancer = function(top, left, timeBetweenSteps){
   this.$node = $('<span class="dancer"></span>');
 
   this._timeBetweenSteps = timeBetweenSteps;
-  if (this._timeBetweenSteps > 100) {
-    this._timeBetweenSteps = 100;
-  }
 
   this.step();
-
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
@@ -21,7 +17,7 @@ Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   var self = this;
-  setTimeout(self.step, self._timeBetweenSteps);
+  setTimeout(function(){self.step()}, self._timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
